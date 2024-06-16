f0128ac2152fd0fba660c07ead08fabe213ea213
aamirkhan06 <114560386+aamirkhan06@users.noreply.github.com>
2024-06-16 20:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstChar(text) {
   // your code here
+	if(text==="")
+	{
+		return "";
+	}
+	let ans="";
+	for(let val of text)
+		{
+			if(val!==" ")
+			{
+				ans=val;
+				break;
+			}
+		}
+	return ans;
 }
 
 // Do not change the code below
